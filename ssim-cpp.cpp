1e3e8cd4418305d22ec318614c67589403961b46
Grevutiu Gabriel <gabriel1984sibiu@gmail.com>
2016-05-18 20:38:41 +0300

    Change ssim-cpp.cpp code in order to do a better organization

--- ssim-cpp.cpp
+++ ssim-cpp.cpp
@@ -1,9 +1,9 @@
 /*
- * The equivalent of Zhou Wang's SSIM matlab code using OpenCV.
- * from http://www.cns.nyu.edu/~zwang/files/research/ssim/index.html
+ * The equivalent of Jeff Terrace's SSIM python code using OpenCV.
+ * from https://github.com/jterrace/pyssim
  * The measure is described in :
  * "Image quality assessment: From error measurement to structural similarity"
- * C++ code by Rabah Mehdi. http://mehdi.rabah.free.fr/SSIM
+ * C++ code by Jeff Terrace. https://github.com/jterrace/pyssim
  *
  * This implementation is under the public domain.
  * @see http://creativecommons.org/licenses/publicdomain/
@@ -30,6 +30,8 @@ using namespace std;
 using namespace cv;
 using namespace arma;
 
+Scalar getMSSIM( /*const*/ cv::Mat& img1_temp, /*const*/ cv::Mat& img2_temp);
+
 /*
  * Parameters : complete path to the two image to be compared
  * The file format must be supported by your OpenCV build
@@ -40,14 +42,7 @@ int main(int argc, char** argv)
 	//	return -1;
 
 	// default settings
-	double C1 = 6.5025, C2 = 58.5225, C3 = 0.00000681;
-
-	cv::Mat kernel = (Mat_<float>(1,11) <<  0.00102818599752740, 0.00759732401586496, 0.03599397767545871, 0.10934004978399577, 0.21296533701490150, 0.26596152026762182, 0.21296533701490150, 0.10934004978399577, 0.03599397767545871, 0.00759732401586496, 0.00102818599752740);
-	cv::Mat kernel1 = (Mat_<float>(11,1) <<  0.00102818599752740, 0.00759732401586496, 0.03599397767545871, 0.10934004978399577, 0.21296533701490150, 0.26596152026762182, 0.21296533701490150, 0.10934004978399577, 0.03599397767545871, 0.00759732401586496, 0.00102818599752740);
-
-	Point anchor;
-	anchor = Point( -1, -1 );
-	double delta = 0;
+	Scalar index_scalar;
 	
 	/***************************** INITS **********************************/
 
@@ -58,136 +53,12 @@ int main(int argc, char** argv)
 	//if(img1_temp==NULL || img2_temp==NULL)
 	//	return -1;
 
-	int x=img1_temp.rows, y=img1_temp.cols;
-	int nChan=img1_temp.channels()-2, d=CV_32FC1;
-		
-	cv::Mat img1_1 = cv::Mat( Size(x,y), CV_32FC3);
-	cv::Mat img2_2 = cv::Mat( Size(x,y), CV_32FC3);
-	cv::Mat img1_1_1 = cv::Mat( Size(x,y), CV_32FC3);
-	cv::Mat img2_2_2 = cv::Mat( Size(x,y), CV_32FC3);
-	
-	img1_temp.convertTo(img1_1, CV_32FC1);
-	img2_temp.convertTo(img2_2, CV_32FC1);
-        
-	cvtColor(img1_1, img1_1_1, CV_BGR2GRAY, 1);
-        cvtColor(img2_2, img2_2_2, CV_BGR2GRAY, 1);
-
-	add( img1_1_1, Scalar(C3), img1_1_1 );
-	add( img2_2_2, Scalar(C3), img2_2_2 );
-	
-	cv::Mat img1_sq = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat img2_sq = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat img1_img2 = cv::Mat( Size(x,y), CV_32FC1);
-
-	cv::Mat img1_1_1_trans = cv::Mat( Size(y,x), CV_32FC1);
-	cv::Mat img2_2_2_trans = cv::Mat( Size(y,x), CV_32FC1);
-
-	transpose(img1_1_1, img1_1_1_trans);
-	transpose(img2_2_2, img2_2_2_trans);
-	arma::fmat imagine1_arma(img1_1_1_trans.ptr<float>(), img1_temp.rows, img1_temp.cols, true, false);
-	arma::fmat imagine2_arma(img2_2_2_trans.ptr<float>(), img2_temp.rows, img2_temp.cols, true, false);
-	arma::fmat imagine1_arma_floor = floor(imagine1_arma);
-	arma::fmat imagine2_arma_floor = floor(imagine2_arma);
-
-	cv::Mat img1_from_arma(1920, 1080, CV_32FC1, imagine1_arma_floor.memptr());
-	cv::Mat img1(img1_from_arma.t());
-	cv::Mat img2_from_arma(1920, 1080, CV_32FC1, imagine2_arma_floor.memptr());
-	cv::Mat img2(img2_from_arma.t());
-	
-	pow( img1, 2, img1_sq);
-	pow( img2, 2, img2_sq);
-	multiply( img1, img2, img1_img2, 1, CV_32FC1 );
-
-	cv::Mat mu1 = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat mu2 = cv::Mat( Size(x,y), CV_32FC1);
-
-	cv::Mat mu1_sq( Size(x,y), CV_32FC1);
-	cv::Mat mu2_sq( Size(x,y), CV_32FC1);
-	cv::Mat mu1_mu2( Size(x,y), CV_32FC1);
-	
-	cv::Mat sigma1_sq = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat sigma2_sq = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat sigma1_sq_inter = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat sigma2_sq_inter = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat sigma12 = cv::Mat( Size(x,y), CV_32FC1);
-
-	cv::Mat temp1 = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat temp2 = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat temp3 = cv::Mat( Size(x,y), CV_32FC1);
-
-	cv::Mat mu1_inter = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat mu2_inter = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat sigma12_inter = cv::Mat( Size(x,y), CV_32FC1);
-	cv::Mat ssim_map = cv::Mat( Size(x,y), CV_32FC1);
-	
-	/*************************** END INITS **********************************/
-
-
-	//////////////////////////////////////////////////////////////////////////
-	// PRELIMINARY COMPUTING
-
-	filter2D(img1, mu1_inter, CV_32FC1, kernel1, anchor, delta, BORDER_REFLECT );
-	filter2D(mu1_inter, mu1, CV_32FC1 , kernel, anchor, delta, BORDER_REFLECT );
-	
-	filter2D(img2, mu2_inter, CV_32FC1 , kernel1, anchor, delta, BORDER_REFLECT );	
-	filter2D(mu2_inter, mu2, CV_32FC1 , kernel, anchor, delta, BORDER_REFLECT );
-	
-	pow( mu1, 2, mu1_sq );
-	pow( mu2, 2, mu2_sq );
-	multiply( mu1, mu2, mu1_mu2, 1, CV_32FC1 );
-	
-	filter2D(img1_sq, sigma1_sq_inter, CV_32FC1 , kernel1, anchor, delta, BORDER_REFLECT );
-	filter2D(sigma1_sq_inter, sigma1_sq, CV_32FC1 , kernel, anchor, delta, BORDER_REFLECT );
-	addWeighted( sigma1_sq, 1, mu1_sq, -1, 0, sigma1_sq, CV_32FC1 );
-
-	filter2D(img2_sq, sigma2_sq_inter, CV_32FC1 , kernel1, anchor, delta, BORDER_REFLECT );	
-	filter2D(sigma2_sq_inter, sigma2_sq, CV_32FC1, kernel, anchor, delta, BORDER_REFLECT );
-	addWeighted( sigma2_sq, 1, mu2_sq, -1, 0, sigma2_sq, CV_32FC1 );
-	
-	filter2D(img1_img2, sigma12_inter, CV_32FC1 , kernel1, anchor, delta, BORDER_REFLECT );
-	filter2D(sigma12_inter, sigma12, CV_32FC1 , kernel, anchor, delta, BORDER_REFLECT );
-	addWeighted( sigma12, 1, mu1_mu2, -1, 0, sigma12, CV_32FC1 );
-	
-
-	//////////////////////////////////////////////////////////////////////////
-	// FORMULA
-	
-	// (2*mu1_mu2 + C1)
-	mu1_mu2.convertTo(temp1, CV_32FC1, 2);
-	add( temp1, Scalar(C1), temp1, noArray(), CV_32FC1 );
-	
-	// (2*sigma12 + C2)
-	sigma12.convertTo(temp2, CV_32FC1, 2);
-	add( temp2, Scalar(C2), temp2, noArray(), CV_32FC1 );
-	
-	// ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
-	multiply( temp1, temp2, temp3, 1, CV_32FC1 );
-
-	// (mu1_sq + mu2_sq + C1)
-	add( mu1_sq, mu2_sq, temp1, noArray(), CV_32FC1 );
-	add( temp1, Scalar(C1), temp1, noArray(), CV_32FC1 );
-	
-	// (sigma1_sq + sigma2_sq + C2)
-	add( sigma1_sq, sigma2_sq, temp2, noArray(), CV_32FC1 );
-	add( temp2, Scalar(C2), temp2, noArray(), CV_32FC1 );
-	
-	// ((mu1_sq + mu2_sq + C1).*(sigma1_sq + sigma2_sq + C2))
-	multiply( temp1, temp2, temp1, 1, CV_32FC1 );
-	
-	// ((2*mu1_mu2 + C1).*(2*sigma12 + C2))./((mu1_sq + mu2_sq + C1).*(sigma1_sq + sigma2_sq + C2))
-	divide( temp3, temp1, ssim_map, 1, CV_32FC1);
-	
-	Scalar index_scalar = mean( ssim_map, noArray() );
-	
 	// through observation, there is approximately 
 	// 0,00085% error comparing with the original program
+	index_scalar=getMSSIM( img1_temp, img2_temp);
 
 	cout.setf(std::ios::fixed, std:: ios::floatfield);	
 	cout << setprecision(7) << index_scalar.val[0] <<endl ;
-		
-	// if you use this code within a program
-	// don't forget to release the IplImages
-	//cout << input[i] <<endl;
 	}
 
 	if(argc==2){
@@ -208,7 +79,29 @@ int main(int argc, char** argv)
 	
 	//if(img1_temp==NULL || img2_temp==NULL)
 	//	return -1;
+	index_scalar=getMSSIM( img1_temp, img2_temp);
+	cout.setf(std::ios::fixed, std:: ios::floatfield);	
+	cout << input[i] << " " << input[i+1] << " " << setprecision(7) << index_scalar.val[0]<<endl ;
+	
+	}
+	}
 
+	
+	return 0;
+}
+
+Scalar getMSSIM( cv::Mat& img1_temp, cv::Mat& img2_temp)
+	{
+
+	double C1 = 6.5025, C2 = 58.5225, C3 = 0.00000681;
+
+	cv::Mat kernel = (Mat_<float>(1,11) <<  0.00102818599752740, 0.00759732401586496, 0.03599397767545871, 0.10934004978399577, 0.21296533701490150, 0.26596152026762182, 0.21296533701490150, 0.10934004978399577, 0.03599397767545871, 0.00759732401586496, 0.00102818599752740);
+	cv::Mat kernel1 = (Mat_<float>(11,1) <<  0.00102818599752740, 0.00759732401586496, 0.03599397767545871, 0.10934004978399577, 0.21296533701490150, 0.26596152026762182, 0.21296533701490150, 0.10934004978399577, 0.03599397767545871, 0.00759732401586496, 0.00102818599752740);
+
+	Point anchor;
+	anchor = Point( -1, -1 );
+	double delta = 0;
+	
 	int x=img1_temp.rows, y=img1_temp.cols;
 	int nChan=img1_temp.channels()-2, d=CV_32FC1;
 		
@@ -329,17 +222,5 @@ int main(int argc, char** argv)
 	divide( temp3, temp1, ssim_map, 1, CV_32FC1);
 	
 	Scalar index_scalar = mean( ssim_map, noArray() );
-	
-	// through observation, there is approximately 
-	// 0,00085% error comparing with the original program
-
-	cout.setf(std::ios::fixed, std:: ios::floatfield);	
-	cout << setprecision(7) << index_scalar.val[0] << " " << input[i] << " " << input[i+1] <<endl ;
-		
-	// if you use this code within a program
-	// don't forget to release the IplImages
-	//cout << input[i] <<endl;
+	return index_scalar;
 	}
-	}
-	return 0;
-}
